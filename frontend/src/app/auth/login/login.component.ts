86630286cabd3231d3d6ef8724874c2977a31f32
YassineIdiri <pro.yassine.idiri@gmail.com>
2026-02-08 22:44:52 +0100

    feature: add login component

--- frontend/src/app/auth/login/login.component.ts
+++ frontend/src/app/auth/login/login.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
 import { Router, RouterModule } from '@angular/router';
 
